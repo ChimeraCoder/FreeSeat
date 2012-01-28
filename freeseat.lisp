34f05d6af7fe9ba4f326cb46660220e1b7e41c80
Zack Newman <znewman01@gmail.com>
2012-01-28 17:57:52 -0500

    Adding first skeleton of program

--- freeseat.lisp
+++ freeseat.lisp
@@ -1,14 +1,37 @@
-Tom and Mary must sit together
-Tom and Josephina must not sit together
+; Tom and Mary must sit together
+; Tom and Josephina must not sit together
 
 
-(defun setrelationship (matrix person1 person2 level)
-  (symmetricallyset matrix person1 person2 level)
-  ;;Return matrix
-  )
+(let ((matrix (make-array '(num-people num-people))) :initial-element 0)
+     (defun set-relationship (person1 person2 level)
+       ;;Return matrix
+       )
 
-(defun symmetricallyset (matrix person1 person2 level)
-  ;;Set both slots in the relationship to level
-  ;;Return matrix
-  )
+     (defun symmetrically-set (person1 person2 level)
+       (setrelationship matrix person1 person2 level)
+       ;;Set both slots in the relationship to level
+       ;;Return matrix
+       )
 
+     (defun get-relationship (person1 person2)
+       ;; Return the relationship weight from person1 to person2
+       )
+
+     (defun get-indexes-sorted ()
+       ;; Return a list of user ids sorted by the sum of the absolute
+       ;; value of the weights in their row, only if they have any
+       ;; restriction
+       )
+
+     (defun seat-by-most-restricted (indexes-sorted groups)
+       ;; Takes the list of indexes sorted by restriction and a list
+       ;; of groups stored as lists and returns the groups populated
+       ;; with people
+       )
+
+     
+       
+
+
+
+(setf (aref 1 2) 5)
