2eb49c0a76c329739d4e2216cf0b6e5e896dbe48
aditya <dev@chimeracoder.net>
2012-01-28 16:36:06 -0500

    Outlined skeleton of setting relationship in matrix

---
+++ freeseat.lisp
@@ -0,0 +1,14 @@
+Tom and Mary must sit together
+Tom and Josephina must not sit together
+
+
+(defun setrelationship (matrix person1 person2 level)
+  (symmetricallyset matrix person1 person2 level)
+  ;;Return matrix
+  )
+
+(defun symmetricallyset (matrix person1 person2 level)
+  ;;Set both slots in the relationship to level
+  ;;Return matrix
+  )
+
