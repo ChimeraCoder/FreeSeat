a6c9701fbb6c8c5040ae7bb69fd13543b02ed0f7
Zack Newman <znewman01@gmail.com>
2012-02-03 20:35:44 -0500

    debugging glued-together version

--- freeseat.lisp
+++ freeseat.lisp
@@ -3,6 +3,7 @@
 (let* ((filename "sampleinput.txt")
        (processed (initial-processing filename)))
   (defvar *tables* (nth 0 processed))
+  (defvar *table-max-size* (car *tables*))
   (defvar *name-to-id* (nth 1 processed))
   (defvar *id-to-name* (nth 2 processed))
   (defvar *num-people* (nth 3 processed))
@@ -13,50 +14,70 @@
 ;;Randomizes the order of the list
 ;;Fix the naming here
 (defun randomize-list (l)
-  (let ((random-group (get-random-group l)))
-    (cons random-group (randomize-list (remove random-group l)))))
+  (if l
+      (let ((random-group (get-random-group l)))
+        (cons random-group (randomize-list (remove random-group l))))
+      '()))
+  
+(defun combination-possible (group1 group2)
+  (defun can-sit-p-p (person1 person2)
+    (not (= (aref *matrix*  person1 person2) *cannot*)))
+  (defun can-sit-p-g (person group)
+    (if group
+        (and (can-sit-p-p person (car group))
+             (can-sit-p-g person (cdr group)))
+        t))
+  (defun can-sit-g-g (group1 group2)
+    (if group1
+        (and (can-sit-p-g (car group1) group2)
+             (can-sit-g-g (cdr group1) group2))
+        t))
+  (can-sit-g-g group1 group2))
 
-;;Given a list l of randomized groups, combine any two groups if they can be combined
-(defvar combine-groups (l)
-  (let ((first-group) (car l)
-        (second-group) (cadr l))
-    (if (and (combination-possible first-group second-group) (<= (+ (calculate-weight first-group) (calculate-weight second-group)) *table-max-size*))
-      (cons (concatenate 'list first-group second-group) (combine-groups (cddr l)))
-      (cons first-group (cons second-group (combine-groups cddr l))))))
 
-(defun mcmc (groups) 
-  (let* ((initial-configuration (combine-groups (randomize-list groups )))
+;;Given a list l of randomized groups, combine any two groups if they
+;;can be combined
+(defun combine-groups (l)
+  (if l 
+      (let ((first-group (car l))
+            (second-group (cadr l)))
+        (if (and (combination-possible first-group second-group)
+                 (<= (+ (calculate-weight first-group)
+                        (calculate-weight second-group))
+                     *table-max-size*))
+            (cons (concatenate 'list first-group second-group)
+                  (combine-groups (cddr l)))
+            (cons first-group
+                  (cons second-group (combine-groups (cddr l))))))
+      '()))
+
+(defun mcmc (groups)
+  (format t "mcmc")
+  (let* ((initial-configuration (combine-groups (randomize-list groups)))
          (group1 (get-random-group initial-configuration))
          (group2 (get-random-group initial-configuration))
-         (switch-result (possibly-switch-groups group1 group2))
-    (if (or (equal group1 group2) (not switch-result)))   ;;This can be optimized/combined in a clever way using or
-      (initial-configuration)
-      (cons (car switch-result) 
-            (cons (cadr switch-result) 
-                  (remove group1 
-                          (remove group2 initial-configuration)))))))
+         (switch-result (possibly-switch-groups group1 group2)))
+    (if (or (equal group1 group2)
+            (not switch-result))   ;;This can be optimized/combined
+        ;;in a clever way using OR
+        (initial-configuration)
+        (cons (car switch-result) 
+              (cons (cadr switch-result) 
+                    (remove group1 
+                            (remove group2 initial-configuration)))))))
+  
 
 
 ;TODO Figure out how to do parallel-let!
-(defun many-parallel-mcmc (groups)
-   (parallel-let ((result1 (iterate-mcmc groups))
-                  (result2 (iterate-mcmc groups))
-                  (result3 (iterate mcmc groups))
-                  (result4 (iterate-mcmc groups)))
-            '(result1 result2 result3 result4)))
+(defun many-parallel-mcmc (groups threads iterations)
+  (if (<= threads 0)
+      '()
+      (cons (iterate-mcmc groups iterations)
+            (many-parallel-mcmc groups threads (1- iterations)))))
 
+(defun iterate-mcmc (groups iterations)
+  (if (<= iterations 0)
+      groups
+      (iterate-mcmc (mcmc groups) (1- iterations))))
 
-
-        
-
-
-
-
-(defun combination-possible 
-  
-  don't compile )
-
-
-
-
-
+(mcmc *groups*)
--- stochastics.lisp
+++ stochastics.lisp
@@ -14,13 +14,12 @@
 
 ;;Calculate the score of a group of people
 (defun calculate-score (group)
-  (let ( (head (car group))
-         (tail (cdr group)))
+  (let ((head (car group))
+        (tail (cdr group)))
     (if tail
-      (+ 
-        (calculate-score-with-everyone-else head tail) ;;The score of the first person with each other person
-        (calculate-score group))                        ;;The scores of every other person EXCEPT the first, with each other
-      '0)))                                               ;;The last person has already been compared to everyone else
+        (+ (calculate-score-with-everyone-else head tail) ;;The score of the first person with each other person
+           (calculate-score group))                        ;;The scores of every other person EXCEPT the first, with each other
+        '0)))                                               ;;The last person has already been compared to everyone else
     
 ;;Given a person A and a list of other people L, calculate the sum of
 ;;the pairwise scores of A with each person in L
@@ -28,9 +27,9 @@
   ;;Sum the list of scores
   (apply '+
          ;;Calculate the pairwise score of the person with every person in the tail
-         (apply 
-           (lambda (other) (pairwise-score person other))
-           tail)))
+         (map 'list
+              (lambda (other) (pairwise-score person other))
+              tail)))
 
 ;;Get the score (hard or soft) between two people. Assume symmetry in
 ;;the true values, but at most one of the two values may be
@@ -39,8 +38,8 @@
   (let ((row-wise (score a b))
         (column-wise (score b a)))
     (if (eq row-wise '0)
-      (row-wise)
-      (column-wise))))
+        (row-wise)
+        column-wise)))
 
 ;; This function is defined separately in case we want to separate the
 ;; matrix into two matrices (hard and soft) later on
@@ -50,6 +49,13 @@
 ;;Get random person from a group
 (defun get-random-person (group)
   (nth (random (length group)) group))
+
+;;Get random group from a list of groups
+(defun get-random-group (groups)
+  (let ((len (length groups)))
+    (if (> len 0)
+        (nth (random len) groups)
+        '())))
 
 ;;Returns a copy of the list without the nth element
 (defun remove-nth (list n)
@@ -61,19 +67,21 @@
 ;;intermediate definitions can be removed, but I've left them in for
 ;;now so it's easy to tweak our algorithm, since this is the core
 ;;heuristic
-(defun possibly-switch-groups (group1 group1)
-  (labels ((person1 (get-random-person group1))
-           (person2 (get-random-person group2))
-           (prescore1 (calculate-score group1))
-           (prescore2 (calculate-score group2))
-           (switched-person-1 (get-random-person group1))
-           (switched-person-2 (get-random-person group2))
-           (hypothetical-table-1 (cons switched-person-1 (remove-nth group2)))
-           (hypothetical-table-2 (cons switched-person-2 (remove-nth group1)))
-           (postscore1 (calculate-score hypothetical-table-1))
-           (postscore2 (calculate-score hypothetical-table-2))
-           (pretotal (+ prescore1 prescore2))
-           (posttotal (+ postscore1 postscore2)))
+(defun possibly-switch-groups (group1 group2)
+  (let* ((person1 (get-random-person group1))
+         (person2 (get-random-person group2))
+         (prescore1 (calculate-score group1))
+         (prescore2 (calculate-score group2))
+         (switched-person-1 (get-random-person group1))
+         (switched-person-2 (get-random-person group2))
+         (hypothetical-table-1 (cons switched-person-1 
+                                     (remove switched-person-2 group2)))
+         (hypothetical-table-2 (cons switched-person-2
+                                     (remove switched-person-1 group1)))
+         (postscore1 (calculate-score hypothetical-table-1))
+         (postscore2 (calculate-score hypothetical-table-2))
+         (pretotal (+ prescore1 prescore2))
+         (posttotal (+ postscore1 postscore2)))
 
     (if (or (> posttotal pretotal ) (> (/ posttotal pretotal) (random 1.0)))
       (cons hypothetical-table-1 hypothetical-table-2)
